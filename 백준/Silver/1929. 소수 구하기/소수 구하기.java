7fd2afc3d983d63b0869e49c4f46b390e9e1a6e9
Somi Nam <somi4219@gmail.com>
2025-10-31 15:30:23 +0900

    [Silver III] Title: 소수 구하기, Time: 316 ms, Memory: 19248 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -6,13 +6,16 @@ public class Main {
         if(n<2) return false; // 0과 1은 소수가 아님
         if(n==2) return true; // 2는 소수임
         if(n%2==0) return false; // 2로 나누어지면 소수가 아님
-
-        for(int i=3; i*i <=n; i+=2){
-            if(n % i == 0){
+        
+        // 2의 배수(짝수)로 나누어진다면 소수가 아님.
+        // 짝수는 무조건 소수 x.
+				// 따라서 +=2씩 증가하면 홀수로만 나눠볼 수 있음.
+        for(int i=3; i*i <=n; i+=2){ // i*i<=n 까지만 비교
+            if(n % i == 0){ // 그 중 나눴을때 0이면 소수가 아님
                 return false;
             }
         }
-        return true;
+        return true; // for문에서 걸러지지 않았다면 소수임
     }
 
     public static void main(String[] args) throws IOException {
